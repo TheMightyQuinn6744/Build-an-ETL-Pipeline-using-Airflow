6539b07833aaac7b1b5ad07e0fbe52c66ef4e288
Quinn Rodney <followthewhiterabit93@gmail.com>
2022-09-19 14:23:56 -0500

    Update ETL_Toll_Data.py
    Add DAG definition

--- ETL_Toll_Data.py
+++ ETL_Toll_Data.py
@@ -1,5 +1,4 @@
 # import the libraries
-
 from datetime import timedelta
 # The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
@@ -21,48 +20,56 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
+#Define the DAG
+dag = DAG(
+    'ETL_Toll_Data',
+    default_args=default_args,
+    description='Apache Airflow Final Assignment',
+    schedule_interval=timedelta(days=1)
+)
+
 #Define tasks
 
 #Create a task to unzip data
 unzip_data = BashOperator(
     task_id='unzip_data',
     bash_command='tar -zxvf /home/project/airflow/dags/finalassignment/tolldata.tgz -C /home/project/airflow/dags/finalassignment',
-    dag=dag
+    dag=dag,
 )
 
 #Create a task to extract data from CSV
 extract_data_from_csv = BashOperator(
     task_id='extract_data_from_csv',
     bash_command='cut -d":" -f1-4 /home/project/airflow/dags/finalassignment/vehicle-data.csv > /home/project/airflow/dags/finalassignment/csv_data.csv',
-    dag=dag
+    dag=dag,
 )
 
 #Create a task to extract data from TSV
 extract_data_from_tsv = BashOperator(
     task_id='extract_data_from_tsv',
     bash_command='cut -f5-7 /home/project/airflow/dags/finalassignment/tollplaza-data.tsv > /home/project/airflow/dags/finalassignment/tsv_data.csv',
-    dag=dag
+    dag=dag,
 )
 
 #Create a task to extract data from a Fixed Width File
 extract_data_from_fixed_width = BashOperator(
     task_id='extract_data_from_fixed_width',
     bash_command='cut -c 1- /home/project/airflow/dags/finalassignment/payment-data.txt > /home/project/airflow/dags/finalassignment/fixed_width_data.csv',
-    dag=dag
+    dag=dag,
 )
 
 #Create a task named consolidate data
 consolidate_data = BashOperator(
     task_id='consolidate data',
     bash_command='paste csv_data.csv tsv_data.csv fixed_width_data.csv > extracted_data.csv',
-    dag=dag
+    dag=dag,
 )
 
 #Create a task named transform data
 transform_and_load = BashOperator(
     task_id='transform',
     bash_command='tr "[a-z]" "[A-Z]" < /home/project/airflow/dags/finalassignment/extracted_data.csv > /home/project/airflow/dags/transformed_data.csv',
-    dag=dag
+    dag=dag,
 )
 
 #task pipeline
